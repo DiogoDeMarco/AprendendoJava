8613d9b9f8bbce461d551426962d025a721224de
Diogo Marco <diogo63466166@alunos.sc.senac.br>
2026-04-02 14:35:07 -0300

    Exercicios

---
+++ src/Aula4desvioCondicional.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class Aula4desvioCondicional {
+
+	public static void main(String[] args) {
+		//exercicio1();
+		//exercicio2();
+		//exercicio3();
+		//exercicio4();
+		exercicio5();
+	}
+
+	static void exercicio1() {
+		Scanner res = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+		String num = res.nextLine();
+		double numR = Double.parseDouble(num);
+		
+		if (numR % 2 == 0) {
+			System.out.print("Seu número é par!");
+		} else {
+			System.out.print("Seu número é impar!");
+		}
+		
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	static void exercicio2() {
+		Scanner res = new Scanner(System.in);
+		System.out.print("Digite o primeiro número: ");
+		String num = res.nextLine();
+		double numero1 = Double.parseDouble(num);
+		
+		System.out.print("Digite o segundo número: ");
+		String num2 = res.nextLine();
+		double numero2 = Double.parseDouble(num2);
+		
+		if (numero1 > numero2) {
+			System.out.print("O número 1 é maior que o 2!");
+		} else {
+			System.out.print("O numero 2 é maior!");
+		}
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	static void exercicio3() {
+		Scanner res = new Scanner(System.in);
+		System.out.print("Digite o número: ");
+		String num = res.nextLine();
+		double numero1 = Double.parseDouble(num);
+		
+		
+		if (numero1 < 0) {
+			System.out.print("O número é negativo!");
+		} else {
+			System.out.print("O número é positivo!");
+		}
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	static void exercicio4() {
+		Scanner res = new Scanner(System.in);
+		System.out.print("Digite a nota final: ");
+		String not = res.nextLine();
+		double nota = Double.parseDouble(not);
+		
+		if (nota >= 6) {
+			System.out.print("Aluno aprovado!");
+		} else {
+			System.out.print("Aluno reprovado!");
+		}	
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	static void exercicio5() {
+		Scanner res = new Scanner(System.in);
+		System.out.print("Digite sua idade: ");
+		String age = res.nextLine();
+		double idade = Double.parseDouble(age);
+		
+		if (idade >= 18) {
+			System.out.print("Permissão pra votar!");
+		} else {
+			System.out.print("Sem permissão pra votar!");
+		}	
+	}
+	
+	
+	
+	
+	
+}
